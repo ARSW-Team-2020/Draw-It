747cc2961a522638cee64d45118355b8bce76871
CarlosGomez380 <53835467+CarlosGomez380@users.noreply.github.com>
2020-03-14 07:10:23 -0500

    Archivo Json

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,15 @@
+var app = (function (){
+
+    function crearSala() {
+        if ($("#Nombre").val()== ""){alert("No hay un nombre especificado"); return false}
+        else{
+
+        }
+
+    }
+
+    return{
+        crearSala:crearSala
+
+    }
+})();
