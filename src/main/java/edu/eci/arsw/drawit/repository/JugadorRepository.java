83dec4794f74fa867766ac21883371eb0c92d814
Juan Sebastián Frásica G <32890741+sebastianfrasic@users.noreply.github.com>
2020-03-17 20:51:14 -0500

    Configuracion base de datos, y JPA inicial

---
+++ src/main/java/edu/eci/arsw/drawit/repository/JugadorRepository.java
@@ -0,0 +1,8 @@
+package edu.eci.arsw.drawit.repository;
+
+import edu.eci.arsw.drawit.model.Jugador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JugadorRepository extends JpaRepository<Jugador, Integer> {
+
+}
--- src/main/java/edu/eci/arsw/drawit/services/DrawItServices.java
+++ src/main/java/edu/eci/arsw/drawit/services/DrawItServices.java
@@ -5,12 +5,21 @@ import edu.eci.arsw.drawit.model.Jugador;
 import edu.eci.arsw.drawit.model.Sala;
 import edu.eci.arsw.drawit.persistence.DrawItException;
 import edu.eci.arsw.drawit.persistence.DrawitPersistence;
+import edu.eci.arsw.drawit.repository.JugadorRepository;
+import edu.eci.arsw.drawit.repository.SalaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service("drawItServices")
 public class DrawItServices {
+
+    @Autowired
+    JugadorRepository jugadorRepository;
+
+    @Autowired
+    SalaRepository salaRepository;
+
 
     @Autowired
     @Qualifier("Action")
